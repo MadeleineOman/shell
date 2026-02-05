11b1da2b449582b0185c3ac9c31e8d90d21f6b1f
Madeleine Oman <madeleine.oman@gmail.com>
2026-02-05 12:28:52 -0500

    ok final touches, ie wrong mv command

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,10 +32,10 @@ mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
 mkdir ./data/raw
-mv ./rawdata ./data/raw
+mv ./rawdata/* ./data/raw/
 
 # 3. List the contents of the ./data/raw directory
-ls ./data/raw 
+ls ./data/raw/
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 mkdir data/processed
@@ -56,7 +56,7 @@ rm data/raw/rawdata/*ipaddr*
 rm data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -R  data/processed/ > /data/inventory.txt
+ls -R  data/processed/ > data/inventory.txt
 
 ###########################################
 
