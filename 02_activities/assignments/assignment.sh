57d9336f423fe5fc9de1ae74d131f9ceddb46d0f
Madeleine Oman <madeleine.oman@gmail.com>
2026-02-05 12:36:09 -0500

    just to update autograder

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -53,7 +53,7 @@ cp data/raw/rawdata/event_log_* data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm data/raw/rawdata/*ipaddr*
-rm data/processed/user_logs/*ipaddr*
+rm data/processed/user_logs/*ipaddr* 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls -R  data/processed/ > data/inventory.txt
